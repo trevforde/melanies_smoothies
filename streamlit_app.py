5da0e11677e3ed8d23aeaf2ff597bf9b5a16a08c
Trevor Forde <30713726+trevforde@users.noreply.github.com>
2026-08-05 16:34:46 -0400

    Update streamlit_app.py
    Adding logic to make requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,3 +49,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(success_message, icon="✅")
+
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
