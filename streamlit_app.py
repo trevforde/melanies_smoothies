d24dc5414b4ffbabcefa1cb90c4d987e4cc218fa
Trevor Forde <30713726+trevforde@users.noreply.github.com>
2026-08-05 17:26:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
+import requests
 import streamlit as st
 from snowflake.snowpark.functions import col
+
 # from snowflake.snowpark.context import get_active_session
 
 
@@ -27,11 +29,16 @@ ingredients_list = st.multiselect(
     max_selections=5,
 )
 if ingredients_list:
-
     # Concat selected fruits as a string
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        # Display more information for each fruit
+        st.header(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
     # Insert Notes: columns not included in the insert command
     # - order_id happens automatically because the ORDERS table uses Unique ID SEQUENCES
@@ -49,19 +56,4 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(success_message, icon="✅")
-
-# import requests  
-# smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")
-# st.text(smoothiefroot_response)
-
-
-import requests
-
-# Clean URL string without brackets
-url = "https://my.smoothiefroot.com/api/fruit/watermelon"
-smoothiefroot_response = requests.get(url)
-
-# Display the result in your standalone app
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
